e31513f82ed0357c0325036b27eb0305491f0236
adrian <adri.latorre@gmail.com>
2020-05-17 15:38:56 +0200

    Fix mapper NP

--- src/mappers/weatherMapper.js
+++ src/mappers/weatherMapper.js
@@ -1,7 +1,8 @@
 export const mapWeather = (data = {}) => {
   return {
-    icon: `http://openweathermap.org/img/wn/${data?.weather[0]?.icon ||
-      '01d'}@2x.png`,
+    icon: `http://openweathermap.org/img/wn/${
+      data?.weather ? data.weather[0].icon : '01d'
+    }@2x.png`,
     temperature: parseInt(data?.main?.temp) || '-'
   }
 }
